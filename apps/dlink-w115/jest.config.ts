0191f8ef1dbe33c8cabe227faf344d3e60288098
Jan Ptacek <janptacek@Jan-MacBook-Pro.local>
2022-12-23 18:49:51 +0100

    Initial commit

---
+++ apps/dlink-w115/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'dlink-w115',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/dlink-w115',
+};
